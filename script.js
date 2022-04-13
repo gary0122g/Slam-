6dc026ebd11ebe7d1a6ee08206e0fdab499ec5c6
gary0122g <gary0122g@gmail.com>
2022-04-13 23:14:03 +0800

    metamask address detection

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const loginButton = document.getElementById("loginButton")
+const userWallet = document.getElementById("userWallet")
 
 function toggleButton(){
-    if (!window.ethereum ){
+    if (!window.ethereum){
         loginButton.innerText = "Metamask is not installed"
         loginButton.style.backgroundColor = "#FFDC33"
         return false
-    }
-    
-    loginButton.addEventListener('click', () => { loginWithMetaMask() })
+    }    
 }
 
-
 loginButton.addEventListener('click',()=>{
-    ethereum.request({method:'eth_requestAccounts'});
-    
+    // ethereum.request({method:'eth_requestAccounts'});
+    getAccount();
+    loginButton.innerText = account;
 })
+
+async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    loginButton.innerHTML = account;
+  }
 
 window.addEventListener('DOMContentLoaded', () => {
     toggleButton()
